685c7a6e0185750134c83d1a19ad7c12b64f33ea
James Woolfenden <james.woolfenden@gmail.com>
2023-04-25 12:36:36 +0100

    new checks

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "name" {
-  type = string
-  default="example-websocket-api"
+  type    = string
+  default = "example-websocket-api"
 }
 
 variable "authorization_type" {
-    type = string
-    default = "AWS_IAM"
+  type    = string
+  default = "AWS_IAM"
 }
 
 variable "stage_name" {
-  type = string
-  default="pike-stage"
+  type    = string
+  default = "pike-stage"
 }
 
 variable "kms_key_id" {
