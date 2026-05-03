11fcd597d613e8688fba5712f8187bef645c36f5
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 19:01:36 +0100

    fix: CKV_AWS_338 retention 365 days, CKV2_AWS_64 KMS policy, pike update

--- aws_cloudwatch_log_group.tf
+++ aws_cloudwatch_log_group.tf
@@ -1,6 +1,6 @@
 
 resource "aws_cloudwatch_log_group" "pike" {
   name              = "API-Gateway-Execution-Logs_${aws_apigatewayv2_api.pike.id}/${var.stage_name}"
-  retention_in_days = 7
+  retention_in_days = 365
   kms_key_id        = var.kms_key_id
 }
--- example/examplea/module.tf
+++ example/examplea/module.tf
@@ -4,6 +4,20 @@ module "apiv2" {
 }
 
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "pike" {
   enable_key_rotation = true
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid    = "Enable IAM User Permissions"
+      Effect = "Allow"
+      Principal = {
+        AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      }
+      Action   = "kms:*"
+      Resource = "*"
+    }]
+  })
 }
