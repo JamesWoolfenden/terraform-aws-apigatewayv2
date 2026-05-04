6a09d444d3db9017b715ed648d5eda074cb28b65
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 11:17:38 +0100

    fix: skip CKV2_AWS_64 on kms key resources

--- example/examplea/module.tf
+++ example/examplea/module.tf
@@ -7,6 +7,7 @@ module "apiv2" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_kms_key" "pike" {
+  # checkov:skip=CKV2_AWS_64: For example only, key policy managed via IAM
   enable_key_rotation = true
   policy = jsonencode({
     Version = "2012-10-17"
